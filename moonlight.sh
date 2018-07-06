0d4ea2b239b574912e61933327672755fba022a3
2play <sprafs@gmail.com>
2018-07-06 15:38:13 +0200

    Update moonlight.sh

--- moonlight.sh
+++ moonlight.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Original Jessie Moonlight Script by TechWizTime revamped by 2Play!
-# Stretch OS Install Supported
+# Stretch OS Install Supported!
 
 echo -e "\n****************************************************************"
 echo -e "   Moonlight Installer (Steam) Script for RetroPie v07.18.2P!"
